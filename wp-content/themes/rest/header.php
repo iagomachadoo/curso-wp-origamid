ba83cd1d5e297e6c15b7a72a2134d221a42b4d98
iagoMachado96 <202204384833@alunos.estacio.br>
2022-11-06 21:29:49 -0300

    commit adicional

--- wp-content/themes/rest/header.php
+++ wp-content/themes/rest/header.php
@@ -2,7 +2,7 @@
 <html lang="pt-br">
 	<head>
 		<meta charset="utf-8">
-		<title>Rest</title>
+		<title><?php bloginfo('name')?></title>
 
 		<link href='https://fonts.googleapis.com/css?family=Alegreya+SC' rel='stylesheet' type='text/css'>
 		<link rel="stylesheet" href="<?php echo get_stylesheet_directory_uri();?>/style.css">
@@ -18,9 +18,9 @@
 		<header>
 			<nav>
 				<ul>
-					<li class="current_page_item"><a href="index.html">Menu</a></li>
-					<li><a href="sobre.html">Sobre</a></li>
-					<li><a href="contato.html">Contato</a></li>
+					<li class="current_page_item"><a href="/">Menu</a></li>
+					<li><a href="/sobre">Sobre</a></li>
+					<li><a href="/contato">Contato</a></li>
 				</ul>
 			</nav>
 
